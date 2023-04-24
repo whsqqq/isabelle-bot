8f8e13c3928706723c3b2faf8eb2f293ce1302e9
whsqqq <dgosceh@gmail.com>
2023-04-24 15:23:01 +0300

    code refactor

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -13,81 +13,93 @@ class Birthday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def bday(self, ctx, action=None, bday=None):
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Cog "Birthday" loaded')
+
+    @commands.group(name='bday', invoke_without_command=True)
+    async def bday(self, ctx):
+        embed = discord.Embed(
+            title="Неправильное действие, укажите, что конкретно вы хотите, `add` `edit` `delete`")
+        embed.set_author(name="Что-то пошло не так...",
+                         icon_url=config.NookIncNegative)
+        embed.set_footer(text=f"Выполнил: {ctx.author}")
+        await ctx.send(embed=embed)
+
+    @bday.command()
+    async def add(self, ctx, birthday=None):
         user_id = str(ctx.author.id)
         with open('text/bdays.json', 'r') as f:
             bday_data = json.load(f)
-        if action == 'add':
-            if not bday or not re.match(r'^\d{1,2}\.\d{1,2}$', bday) or int(bday.split(".")[0]) > 31 or int(bday.split(".")[1]) > 12:
-                embed = discord.Embed(
-                    title="Неправильный формат данных. Учтите, что формат должен быть `число.месяц`. Пример: `!bday add 01.01`.")
-                embed.set_author(name="Что-то пошло не так...",
-                                 icon_url=config.NookIncNegative)
-                embed.set_footer(text=f"Выполнил: {ctx.author}")
-                await ctx.send(embed=embed)
-                return
-            bday_data[user_id] = bday
-            with open('text/bdays.json', 'w') as f:
-                json.dump(bday_data, f)
-            embed = discord.Embed(title="Дата рождения добавлена в ваш паспорт!")
-            embed.set_author(name="Изменения в паспорт внесены успешно!",
-                             icon_url=config.NookIncPositive)
-            embed.set_footer(text=f"Выполнил: {ctx.author}")
-            await ctx.send(embed=embed)
-        elif action == 'edit':
-            if not bday or not re.match(r'^\d{1,2}\.\d{1,2}$', bday) or int(bday.split(".")[0]) > 31 or int(bday.split(".")[1]) > 12:
-                embed = discord.Embed(
-                    title="Неправильный формат данных. Учтите, что формат должен быть `число.месяц`. Пример: `!bday add 01.01`.")
-                embed.set_author(name="Что-то пошло не так...",
-                                 icon_url=config.NookIncNegative)
-                embed.set_footer(text=f"Выполнил: {ctx.author}")
-                await ctx.send(embed=embed)
-                return
-            if user_id not in bday_data:
-                embed = discord.Embed(
-                    title="Вы ещё не указали свой день рождения. Чтобы это сделать, вы можете воспользоваться командой `!bday add`")
-                embed.set_author(name="Что-то пошло не так...",
-                                 icon_url=config.NookIncNegative)
-                embed.set_footer(text=f"Выполнил: {ctx.author}")
-                await ctx.send(embed=embed)
-                return
-            bday_data[user_id] = bday
-            with open('text/bdays.json', 'w') as f:
-                json.dump(bday_data, f)
-            embed = discord.Embed(title="Вы успешно изменили свою дату рождения")
-            embed.set_author(name="Изменения в паспорт внесены успешно!",
-                             icon_url=config.NookIncNeutral)
-            embed.set_footer(text=f"Выполнил: {ctx.author}")
-            await ctx.send(embed=embed)
-        elif action == 'delete':
-            if user_id not in bday_data:
-                embed = discord.Embed(
-                    title="Вы ещё не указали свой день рождения. Чтобы это сделать, вы можете воспользоваться командой `!bday add`")
-                embed.set_author(name="Что-то пошло не так...",
-                                 icon_url=config.NookIncNegative)
-                embed.set_footer(text=f"Выполнил: {ctx.author}")
-                await ctx.send(embed=embed)
-                return
-            del bday_data[user_id]
-            with open('text/bdays.json', 'w') as f:
-                json.dump(bday_data, f)
-            embed = discord.Embed(title="Вы успешно удалили свою дату рождения!")
-            embed.set_author(name="Изменения в паспорт внесены успешно!",
-                             icon_url=config.NookIncPositive)
-            embed.set_footer(text=f"Выполнил: {ctx.author}")
-            await ctx.send(embed=embed)
-        else:
+        if not birthday or not re.match(r'^\d{1,2}\.\d{1,2}$', birthday):
             embed = discord.Embed(
-                title="Неправильное действие, укажите, что конкретно вы хотите, `add` `edit` `delete`")
+                title="Неправильный формат данных. Учтите, что формат должен быть `число.месяц`. Пример: `!bday add 01.01`.")
             embed.set_author(name="Что-то пошло не так...",
                              icon_url=config.NookIncNegative)
             embed.set_footer(text=f"Выполнил: {ctx.author}")
             await ctx.send(embed=embed)
+            return
+        bday_data[user_id] = birthday
+        with open('text/bdays.json', 'w') as f:
+            json.dump(bday_data, f)
+        embed = discord.Embed(title="Дата рождения добавлена в ваш паспорт!")
+        embed.set_author(name="Изменения в паспорт внесены успешно!",
+                         icon_url=config.NookIncPositive)
+        embed.set_footer(text=f"Выполнил: {ctx.author}")
+        await ctx.send(embed=embed)
 
-        @commands.Cog.listener()
-        async def on_ready(self):
-            print('Cog "Birthday" loaded')
+    @bday.command()
+    async def edit(self, ctx, birthday=None):
+        user_id = str(ctx.author.id)
+        with open('text/bdays.json', 'r') as f:
+            bday_data = json.load(f)
+        if not birthday or not re.match(r'^\d{1,2}\.\d{1,2}$', birthday):
+            embed = discord.Embed(
+                title="Неправильный формат данных. Учтите, что формат должен быть `число.месяц`. Пример: `!bday add 01.01`.")
+            embed.set_author(name="Что-то пошло не так...",
+                             icon_url=config.NookIncNegative)
+            embed.set_footer(text=f"Выполнил: {ctx.author}")
+            await ctx.send(embed=embed)
+            return
+        if user_id not in bday_data:
+            embed = discord.Embed(
+                title="Вы ещё не указали свой день рождения. Чтобы это сделать, вы можете воспользоваться командой `!bday add`")
+            embed.set_author(name="Что-то пошло не так...",
+                             icon_url=config.NookIncNegative)
+            embed.set_footer(text=f"Выполнил: {ctx.author}")
+            await ctx.send(embed=embed)
+            return
+        bday_data[user_id] = birthday
+        with open('text/bdays.json', 'w') as f:
+            json.dump(bday_data, f)
+        embed = discord.Embed(title="Вы успешно изменили свою дату рождения")
+        embed.set_author(name="Изменения в паспорт внесены успешно!",
+                         icon_url=config.NookIncNeutral)
+        embed.set_footer(text=f"Выполнил: {ctx.author}")
+        await ctx.send(embed=embed)
+
+    @bday.command()
+    async def delete(self, ctx):
+        user_id = str(ctx.author.id)
+        with open('text/bdays.json', 'r') as f:
+            bday_data = json.load(f)
+
+        if user_id not in bday_data:
+            embed = discord.Embed(
+                title="Вы ещё не указали свой день рождения. Чтобы это сделать, вы можете воспользоваться командой `!bday add`")
+            embed.set_author(name="Что-то пошло не так...",
+                             icon_url=config.NookIncNegative)
+            embed.set_footer(text=f"Выполнил: {ctx.author}")
+            await ctx.send(embed=embed)
+            return
+        del bday_data[user_id]
+        with open('text/bdays.json', 'w') as f:
+            json.dump(bday_data, f)
+        embed = discord.Embed(title="Вы успешно удалили свою дату рождения!")
+        embed.set_author(name="Изменения в паспорт внесены успешно!",
+                         icon_url=config.NookIncPositive)
+        embed.set_footer(text=f"Выполнил: {ctx.author}")
+        await ctx.send(embed=embed)
 
 
 async def setup(bot):
